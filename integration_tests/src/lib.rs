94a4df56a3cf02951350f33402b71bec3471080e
Adam Petro <adam.petro@shopify.com>
2025-03-12 11:50:03 -0400

    Remove `wasm-merge` and do it ourself with `walrus`

--- integration_tests/src/lib.rs
+++ integration_tests/src/lib.rs
@@ -1,20 +1,10 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use std::process::Command;
 use std::sync::LazyLock;
 
 fn workspace_root() -> std::path::PathBuf {
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     std::path::PathBuf::from(manifest_dir).join("..")
-}
-
-fn trampoline_wasm() -> std::path::PathBuf {
-    let workspace_root = workspace_root();
-    workspace_root.join("target/trampoline.wasm")
-}
-
-/// Builds the trampoline.wasm file from the trampoline.wat file
-fn build_trampoline() -> Result<()> {
-    shopify_function_wasm_api_trampoline::generate_trampoline()?.emit_wasm_file(trampoline_wasm())
 }
 
 /// Builds the provider library to a `.wasm` file
@@ -55,56 +45,28 @@ fn build_example(name: &str) -> Result<()> {
     Ok(())
 }
 
-/// Merges the example with the trampoline
-fn merge_example(name: &str) -> Result<()> {
+/// Applies the trampoline to the example
+fn apply_trampoline_to_example(name: &str) -> Result<()> {
     let workspace_root = workspace_root();
     let examples_dir = workspace_root.join("target/wasm32-wasip1/release/examples");
-    let trampoline_wasm = trampoline_wasm();
-    let trampoline_wasm = trampoline_wasm
-        .to_str()
-        .context("Failed to convert path to string")?;
     let example_path = examples_dir.join(name).with_extension("wasm");
     let merged_path = example_path.with_extension("merged.wasm");
-    let merged_path = merged_path
-        .to_str()
-        .context("Failed to convert path to string")?;
-    let example_path = example_path
-        .to_str()
-        .context("Failed to convert path to string")?;
-
-    let status = Command::new("wasm-merge")
-        .args([
-            "--enable-bulk-memory",
-            "--enable-multimemory",
-            trampoline_wasm,
-            shopify_function_wasm_api_trampoline::PROVIDER_MODULE_NAME,
-            example_path,
-            "function",
-            "-o",
-            merged_path,
-        ])
-        .current_dir(workspace_root)
-        .status()?;
-
-    if !status.success() {
-        anyhow::bail!(status);
-    }
+    let mut merged_module =
+        shopify_function_wasm_api_trampoline::trampoline_existing_module(example_path)?;
+    merged_module.emit_wasm_file(merged_path)?;
 
     Ok(())
 }
 
-static BUILD_TRAMPOLINE_RESULT: LazyLock<Result<()>> = LazyLock::new(build_trampoline);
 static BUILD_PROVIDER_RESULT: LazyLock<Result<()>> = LazyLock::new(build_provider);
 
 /// Builds the trampoline, provider, and example, and merges the example with the trampoline
 pub fn prepare_example(name: &str) -> Result<()> {
-    BUILD_TRAMPOLINE_RESULT
-        .as_ref()
-        .map_err(|e| anyhow::anyhow!("Failed to build trampoline: {}", e))?;
     BUILD_PROVIDER_RESULT
         .as_ref()
         .map_err(|e| anyhow::anyhow!("Failed to build provider: {}", e))?;
     build_example(name).map_err(|e| anyhow::anyhow!("Failed to build example: {}", e))?;
-    merge_example(name).map_err(|e| anyhow::anyhow!("Failed to merge example: {}", e))?;
+    apply_trampoline_to_example(name)
+        .map_err(|e| anyhow::anyhow!("Failed to apply trampoline: {}", e))?;
     Ok(())
 }
--- trampoline/src/lib.rs
+++ trampoline/src/lib.rs
@@ -1,14 +1,10 @@
-use walrus::{Module, ModuleConfig, ValType};
+use std::path::Path;
+use walrus::{Module, ValType};
 
 pub const PROVIDER_MODULE_NAME: &str = concat!("shopify_function_v", env!("CARGO_PKG_VERSION"));
 
-pub fn generate_trampoline() -> walrus::Result<Module> {
-    // Construct a new Walrus module.
-    let config = ModuleConfig::new();
-    let mut module = Module::with_config(config);
-
-    // Import the `memory` of the consumer module (aka the guest code).
-    let (_memory, _) = module.add_import_memory("function", "memory", false, false, 1, None, None);
+pub fn trampoline_existing_module(path: impl AsRef<Path>) -> walrus::Result<Module> {
+    let mut module = Module::from_file(path)?;
 
     // Import the `_shopify_function_input_get` function.
     let input_get_type = module.types.add(&[], &[ValType::I64]);
@@ -18,8 +14,12 @@ pub fn generate_trampoline() -> walrus::Result<Module> {
         input_get_type,
     );
 
-    // Export the `shopify_function_input_get` function.
-    module.exports.add("shopify_function_input_get", input_get);
+    let imported_input_get = module
+        .imports
+        .get_func(PROVIDER_MODULE_NAME, "shopify_function_input_get")?;
+    module.replace_imported_func(imported_input_get, |(builder, _arg_locals)| {
+        builder.func_body().call(input_get);
+    })?;
 
     Ok(module)
 }
