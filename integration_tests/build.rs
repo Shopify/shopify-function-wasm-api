131d1bcacc171de460654e2be4fb9994bac6b400
Adam Petro <adam.petro@shopify.com>
2025-03-19 11:25:26 -0400

    remove build script that isn't needed

--- integration_tests/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo::rerun-if-changed=../trampoline.wat");
-    println!("cargo::rerun-if-changed=../provider");
-    println!("cargo::rerun-if-changed=../api");
-    println!("cargo::rerun-if-changed=build.rs");
-}
