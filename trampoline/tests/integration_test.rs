5be2ee9512ff1522c14a5395900fdf641c8dfed9
Andrew Hassan <andrew.hassan@shopify.com>
2025-03-26 12:13:20 -0400

    Use Command::cargo_bin

--- trampoline/tests/integration_test.rs
+++ trampoline/tests/integration_test.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use assert_cmd::prelude::*;
 use std::path::PathBuf;
 use std::process::Command;
 use std::sync::LazyLock;
@@ -34,7 +35,6 @@ fn test_trampoline_cli() -> Result<()> {
     ECHO_EXAMPLE
         .as_ref()
         .map_err(|e| anyhow::anyhow!("Failed to build example: {}", e))?;
-    println!("workspace_root: {:?}", workspace_root());
     let workspace_root = workspace_root();
     let input_path = workspace_root
         .join("target/wasm32-wasip1/release/examples")
@@ -44,17 +44,13 @@ fn test_trampoline_cli() -> Result<()> {
         .join("echo.merged.wasm");
 
     // Run the trampoline-cli on the example
-    let status = Command::new("cargo")
+    let status = Command::cargo_bin(env!("CARGO_PKG_NAME"))?
         .args([
-            "run",
-            "--release",
-            "--",
             "--input",
             input_path.to_str().unwrap(),
             "--output",
             output_path.to_str().unwrap(),
         ])
-        .current_dir(workspace_root.join("trampoline"))
         .status()?;
 
     assert!(status.success(), "Trampoline CLI failed to run");
