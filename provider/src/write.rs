9228271695edd658a9e174e1620df40acf34643e
Adam Petro <adam.petro@shopify.com>
2025-03-27 19:16:06 -0400

    Make `WriteContext` more testable

--- provider/src/write.rs
+++ provider/src/write.rs
@@ -13,6 +13,76 @@ struct WriteContext {
     state: State,
 }
 
+impl WriteContext {
+    fn write_bool(&mut self, bool: bool) -> WriteResult {
+        let result = self.state.write_non_string_scalar();
+        if result != WriteResult::Ok {
+            return result;
+        }
+        encode::write_bool(&mut self.bytes, bool).unwrap(); // infallible unwrap
+        WriteResult::Ok
+    }
+
+    fn write_nil(&mut self) -> WriteResult {
+        let result = self.state.write_non_string_scalar();
+        if result != WriteResult::Ok {
+            return result;
+        }
+        encode::write_nil(&mut self.bytes).unwrap(); // infallible unwrap
+        WriteResult::Ok
+    }
+
+    fn write_i32(&mut self, int: i32) -> WriteResult {
+        let result = self.state.write_non_string_scalar();
+        if result != WriteResult::Ok {
+            return result;
+        }
+        encode::write_sint(&mut self.bytes, int as i64).unwrap(); // infallible unwrap
+        WriteResult::Ok
+    }
+
+    fn write_f64(&mut self, float: f64) -> WriteResult {
+        let result = self.state.write_non_string_scalar();
+        if result != WriteResult::Ok {
+            return result;
+        }
+        encode::write_f64(&mut self.bytes, float).unwrap(); // infallible unwrap
+        WriteResult::Ok
+    }
+
+    fn allocate_utf8_str(&mut self, len: usize) -> (WriteResult, *const u8) {
+        let result = self.state.write_string();
+        if result != WriteResult::Ok {
+            return (result, std::ptr::null());
+        }
+        encode::write_str_len(&mut self.bytes, len as u32).unwrap(); // infallible unwrap
+        let original_len = self.bytes.as_slice().len();
+        // fill in the new bytes with zeros; the trampoline will copy the string to overwrite them
+        self.bytes.as_mut_vec().resize(original_len + len, 0);
+        (
+            WriteResult::Ok,
+            self.bytes.as_slice()[original_len..].as_ptr(),
+        )
+    }
+
+    fn start_object(&mut self, len: usize) -> WriteResult {
+        let result = self.state.start_object(len);
+        if result != WriteResult::Ok {
+            return result;
+        }
+        encode::write_map_len(&mut self.bytes, len as u32).unwrap(); // infallible unwrap
+        WriteResult::Ok
+    }
+
+    fn finish_object(&mut self) -> WriteResult {
+        let result = self.state.finish_object();
+        if result != WriteResult::Ok {
+            return result;
+        }
+        WriteResult::Ok
+    }
+}
+
 fn write_context_from_raw(context: WriteContextPtr) -> NonNull<WriteContext> {
     unsafe { NonNull::new_unchecked(context as _) }
 }
@@ -25,88 +95,45 @@ extern "C" fn shopify_function_output_new() -> WriteContextPtr {
 #[export_name = "_shopify_function_output_new_bool"]
 extern "C" fn shopify_function_output_new_bool(context: WriteContextPtr, bool: u32) -> WriteResult {
     let mut context = write_context_from_raw(context);
-    let WriteContext { bytes, state } = unsafe { &mut context.as_mut() };
-    let result = state.write_non_string_scalar();
-    if result != WriteResult::Ok {
-        return result;
-    }
-    encode::write_bool(bytes, bool != 0).unwrap(); // infallible unwrap
-    WriteResult::Ok
+    unsafe { context.as_mut().write_bool(bool != 0) }
 }
 
 #[export_name = "_shopify_function_output_new_null"]
 extern "C" fn shopify_function_output_new_null(context: WriteContextPtr) -> WriteResult {
     let mut context = write_context_from_raw(context);
-    let WriteContext { bytes, state } = unsafe { &mut context.as_mut() };
-    let result = state.write_non_string_scalar();
-    if result != WriteResult::Ok {
-        return result;
-    }
-    encode::write_nil(bytes).unwrap(); // infallible unwrap
-    WriteResult::Ok
+    unsafe { context.as_mut().write_nil() }
 }
 
 #[export_name = "_shopify_function_output_new_i32"]
 extern "C" fn shopify_function_output_new_i32(context: WriteContextPtr, int: i32) -> WriteResult {
     let mut context = write_context_from_raw(context);
-    let WriteContext { bytes, state } = unsafe { &mut context.as_mut() };
-    let result = state.write_non_string_scalar();
-    if result != WriteResult::Ok {
-        return result;
-    }
-    encode::write_sint(bytes, int as i64).unwrap(); // infallible unwrap
-    WriteResult::Ok
+    unsafe { context.as_mut().write_i32(int) }
 }
 
 #[export_name = "_shopify_function_output_new_f64"]
 extern "C" fn shopify_function_output_new_f64(context: WriteContextPtr, float: f64) -> WriteResult {
     let mut context = write_context_from_raw(context);
-    let WriteContext { bytes, state } = unsafe { &mut context.as_mut() };
-    let result = state.write_non_string_scalar();
-    if result != WriteResult::Ok {
-        return result;
-    }
-    encode::write_f64(bytes, float).unwrap(); // infallible unwrap
-    WriteResult::Ok
+    unsafe { context.as_mut().write_f64(float) }
 }
 
 /// The most significant 32 bits are the result, the least significant 32 bits are the pointer.
 #[export_name = "_shopify_function_output_new_utf8_str"]
 extern "C" fn shopify_function_output_new_utf8_str(context: WriteContextPtr, len: usize) -> u64 {
-    let (result, ptr): (WriteResult, *const u8) = {
-        let mut context = write_context_from_raw(context);
-        let WriteContext { bytes, state } = unsafe { &mut context.as_mut() };
-        let result = state.write_string();
-        if result != WriteResult::Ok {
-            (result, std::ptr::null())
-        } else {
-            encode::write_str_len(bytes, len as u32).unwrap(); // infallible unwrap
-            let original_len = bytes.as_slice().len();
-            // fill in the new bytes with zeros; the trampoline will copy the string to overwrite them
-            bytes.as_mut_vec().resize(original_len + len, 0);
-            (WriteResult::Ok, bytes.as_slice()[original_len..].as_ptr())
-        }
-    };
+    let mut context = write_context_from_raw(context);
+    let (result, ptr) = unsafe { context.as_mut().allocate_utf8_str(len) };
     ((result as u64) << 32) | ptr as u64
 }
 
 #[export_name = "_shopify_function_output_new_object"]
 fn shopify_function_output_new_object(context: WriteContextPtr, len: usize) -> WriteResult {
     let mut context = write_context_from_raw(context);
-    let WriteContext { bytes, state } = unsafe { &mut context.as_mut() };
-    let result = state.start_object(len);
-    if result != WriteResult::Ok {
-        return result;
-    }
-    encode::write_map_len(bytes, len as u32).unwrap(); // infallible unwrap
-    WriteResult::Ok
+    unsafe { context.as_mut().start_object(len) }
 }
 
 #[export_name = "_shopify_function_output_finish_object"]
 extern "C" fn shopify_function_output_finish_object(context: WriteContextPtr) -> WriteResult {
     let mut context = write_context_from_raw(context);
-    let WriteContext { state, .. } = unsafe { &mut context.as_mut() };
-    state.finish_object()
+    unsafe { context.as_mut().finish_object() }
 }
 
 #[export_name = "_shopify_function_output_finalize"]
@@ -128,4 +155,97 @@ extern "C" fn shopify_function_output_finalize(context: WriteContextPtr) -> Writ
     }
     let _ = unsafe { Box::from_raw(context.as_ptr()) }; // drop the context
     WriteResult::Ok
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn bytes_to_json(bytes: &[u8]) -> serde_json::Value {
+        let value = rmp_serde::from_slice(bytes).unwrap();
+        serde_json::from_value(value).unwrap()
+    }
+
+    fn write_key(context: &mut WriteContext, key: &str) -> WriteResult {
+        let (result, ptr) = context.allocate_utf8_str(key.len());
+        if result != WriteResult::Ok {
+            return result;
+        }
+        unsafe { std::ptr::copy_nonoverlapping(key.as_ptr(), ptr as *mut u8, key.len()) };
+        WriteResult::Ok
+    }
+
+    #[test]
+    fn test_write_context_bool() {
+        let mut context = WriteContext::default();
+        context.write_bool(true);
+        assert_eq!(context.state, State::End);
+        assert_eq!(context.write_bool(true), WriteResult::ValueAlreadyWritten);
+        let json = bytes_to_json(context.bytes.as_slice());
+        assert_eq!(json, serde_json::json!(true));
+    }
+
+    #[test]
+    fn test_write_context_null() {
+        let mut context = WriteContext::default();
+        context.write_nil();
+        assert_eq!(context.state, State::End);
+        assert_eq!(context.write_nil(), WriteResult::ValueAlreadyWritten);
+        let json = bytes_to_json(context.bytes.as_slice());
+        assert_eq!(json, serde_json::json!(null));
+    }
+
+    #[test]
+    fn test_write_context_i32() {
+        let mut context = WriteContext::default();
+        context.write_i32(42);
+        assert_eq!(context.state, State::End);
+        assert_eq!(context.write_i32(42), WriteResult::ValueAlreadyWritten);
+        let json = bytes_to_json(context.bytes.as_slice());
+        assert_eq!(json, serde_json::json!(42));
+    }
+
+    #[test]
+    fn test_write_context_f64() {
+        let mut context = WriteContext::default();
+        context.write_f64(42.0);
+        assert_eq!(context.state, State::End);
+        assert_eq!(context.write_f64(42.0), WriteResult::ValueAlreadyWritten);
+        let json = bytes_to_json(context.bytes.as_slice());
+        assert_eq!(json, serde_json::json!(42.0));
+    }
+
+    #[test]
+    fn test_write_context_utf8_str() {
+        let mut context = WriteContext::default();
+        let s = "hello";
+        let (result, ptr) = context.allocate_utf8_str(s.len());
+        assert_eq!(result, WriteResult::Ok);
+        unsafe {
+            std::ptr::copy_nonoverlapping(s.as_ptr(), ptr as *mut u8, s.len());
+        }
+        let (result, ptr) = context.allocate_utf8_str(s.len());
+        assert_eq!(result, WriteResult::ValueAlreadyWritten);
+        assert_eq!(ptr, std::ptr::null());
+        let json = bytes_to_json(context.bytes.as_slice());
+        assert_eq!(json, serde_json::json!(s));
+    }
+
+    #[test]
+    fn test_write_context_object() {
+        let mut context = WriteContext::default();
+        let result = context.start_object(2);
+        assert_eq!(result, WriteResult::Ok);
+        assert_eq!(context.write_bool(true), WriteResult::ExpectedKey);
+        assert_eq!(write_key(&mut context, "key"), WriteResult::Ok);
+        assert_eq!(context.write_bool(false), WriteResult::Ok);
+        assert_eq!(context.finish_object(), WriteResult::ObjectLengthError);
+        assert_eq!(write_key(&mut context, "other_key"), WriteResult::Ok);
+        assert_eq!(context.start_object(0), WriteResult::Ok);
+        assert_eq!(context.finish_object(), WriteResult::Ok);
+        assert_eq!(context.finish_object(), WriteResult::Ok);
+        assert_eq!(context.start_object(0), WriteResult::ValueAlreadyWritten);
+        let json = bytes_to_json(context.bytes.as_slice());
+        assert_eq!(json, serde_json::json!({ "key": false, "other_key": {} }));
+    }
 }
