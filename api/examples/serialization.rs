db15196c799ec533568ddfc106ce67c99ba2626c
Andrew Hassan <andrew.hassan@shopify.com>
2025-04-15 13:43:36 -0400

    Define a Serialize trait and base implementations

---
+++ api/examples/serialization.rs
@@ -0,0 +1,55 @@
+use std::{collections::HashMap, error::Error};
+
+use shopify_function_wasm_api::{write::Error as WriteError, write::Serialize, Context};
+
+struct MyData {
+    my_string: String,
+    my_i32: i32,
+    my_f64: f64,
+    my_bool: bool,
+    my_vec: Vec<i32>,
+    my_hash_map: HashMap<String, i32>,
+}
+
+impl Serialize for MyData {
+    fn serialize(&self, context: &mut Context) -> Result<(), WriteError> {
+        context.write_object(
+            |context| {
+                context.write_utf8_str("my_string")?;
+                self.my_string.serialize(context)?;
+                context.write_utf8_str("my_i32")?;
+                self.my_i32.serialize(context)?;
+                context.write_utf8_str("my_f64")?;
+                self.my_f64.serialize(context)?;
+                context.write_utf8_str("my_bool")?;
+                self.my_bool.serialize(context)?;
+                context.write_utf8_str("my_vec")?;
+                self.my_vec.serialize(context)?;
+                context.write_utf8_str("my_hash_map")?;
+                self.my_hash_map.serialize(context)?;
+
+                Ok(())
+            },
+            6,
+        )?;
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let my_data = MyData {
+        my_string: "Hello, world!".to_string(),
+        my_i32: 42,
+        my_f64: 3.14,
+        my_bool: true,
+        my_vec: vec![1, 2, 3],
+        my_hash_map: HashMap::from([("foo".to_string(), 1), ("bar".to_string(), 2)]),
+    };
+
+    let mut context = Context::new();
+
+    my_data.serialize(&mut context)?;
+    context.finalize_output()?;
+
+    Ok(())
+}
