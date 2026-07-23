e9ae7b5efba6cff4a7ad593ca1beb777402413d7
Adam Petro <adam.petro@shopify.com>
2026-07-23 15:06:19 -0400

    autoresearch: test 8 preallocated string spans
    Assisted-By: devx/4abf3f2d-b095-47ae-9a27-38cf2be74da2

--- provider/src/read/nav.rs
+++ provider/src/read/nav.rs
@@ -23,7 +23,7 @@ impl InputState {
     pub(crate) fn preallocated() -> Self {
         Self {
             root: 0,
-            strings: Vec::with_capacity(16),
+            strings: Vec::with_capacity(8),
             shape_keys: Vec::with_capacity(32),
             shapes: Vec::with_capacity(8),
         }
