2e3298338e03d5871ee1297f6b166e21d98c59a8
Adam Petro <adam.petro@shopify.com>
2026-07-23 14:05:06 -0400

    autoresearch: enlarge container metadata cache
    Assisted-By: devx/4abf3f2d-b095-47ae-9a27-38cf2be74da2

--- provider/src/read/nav.rs
+++ provider/src/read/nav.rs
@@ -5,7 +5,7 @@ use shopify_function_wasm_api_core::read::{ErrorCode, NanBox};
 use std::collections::HashMap;
 
 const MAX_DEPTH: u32 = 128;
-const CONTAINER_CACHE_LEN: usize = 8;
+const CONTAINER_CACHE_LEN: usize = 16;
 const CURSOR_CACHE_LEN: usize = 4;
 const INVALID_OFFSET: u32 = u32::MAX;
 
